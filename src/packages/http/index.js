0083de347a3255370311302a82141660a6cb0381
Rafael Raupp <raupperino@gmail.com>
2018-08-07 17:04:34 -0300

    Cleaning code

--- src/packages/http/index.js
+++ src/packages/http/index.js
@@ -4,39 +4,29 @@ import env from '@env'
 
 export default {
 
-    install (Vue, options) {
+    install(Vue, options) {
 
         // Add request interceptor
         axios.interceptors.request.use(
-            // TODO set loading animation
-            // NProgress.start()
-
-            function (config) {
-                config.url = env.API_URL + config.url
-                return config;
+            (request) => {
+                request.url = env.API_URL + request.url
+                return request;
             },
-            function (error) {
-                // Do something with request error
+            (error) => {
                 return Promise.reject(error);
             }
         )
 
         // Add response interceptor
-        axios.interceptors.response.use((response) => {
-
-            // TODO finish animation
-            // NProgress.done()
-
-            return response.data
-
-        }, (error) => {
-
-            // TODO finish animation
-            // NProgress.done()
-            // TODO check for invalid token
-
-            return Promise.reject(error)
-        });
+        axios.interceptors.response.use(
+            (response) => {
+                return response.data
+            },
+            (error) => {
+                // TODO add error to state
+                return Promise.reject(error)
+            }
+        )
 
         Vue.prototype.$http = Vue.http = axios
         Vue.prototype.$axios = Vue.axios = axios
