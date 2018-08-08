16f94e39cf132b1b3364c62a742e98d0fb398751
Rafael Raupp <raupperino@gmail.com>
2018-08-08 10:51:50 -0300

    Improvements on register

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="box">
         <div class="box-body">
-            <v-form @submit="submit" v-if="this.form === 0" v-model="valid">
+            <v-form @submit="submit">
 
                 <v-text-field label="Email"
                               name="email"
@@ -13,17 +13,17 @@
                 <v-text-field name="password"
                               label="Password"
                               type="password"
+                              ref="password"
                               v-model="password"
-                              v-validate="'required|min:6|confirmed:confirm'"
+                              v-validate="'required|min:6'"
                               :rules="[errors.first('password') || true]"
                               required></v-text-field>
 
                 <v-text-field name="confirm"
                               label="Password Confirmation"
                               type="password"
-                              ref="confirm"
                               v-model="confirm"
-                              v-validate="'required|min:6'"
+                              v-validate="'required|min:6|confirmed:password'"
                               :rules="[errors.first('confirm') || true]"
                               required></v-text-field>
 
@@ -34,47 +34,23 @@
                         <router-link to="#" class="text-sm">Forgot you password?</router-link>
                     </div>
 
-                    <button class="btn btn-primary">Register</button>
+                    <button class="btn btn-primary" :disabled="errors.any()">Register</button>
                 </div>
 
             </v-form>
-
-            <form @submit="submit" v-if="this.form === 1">
-
-                <v-text-field v-model="username"
-                              label="Epic Account Username"></v-text-field>
-
-                <!--<div class="footer">-->
-                <!--<div>-->
-                <!--<a href="javascript:void(0)" class="text-sm" @click="form = 0">-->
-                <!--<i class="fa fa-chevron-left" style="display: inline"></i>-->
-                <!--Back-->
-                <!--</a>-->
-                <!--</div>-->
-
-                <!--<button class="btn btn-primary">Register</button>-->
-                <!--</div>-->
-            </form>
         </div>
     </div>
 </template>
 
 <script>
-    import {Validator} from 'vee-validate'
-
     export default {
         components: {},
 
         data() {
             return {
-                form: 0,
-
                 email: null,
                 password: null,
                 confirm: null,
-                username: null,
-
-                valid: false,
             }
         },
 
@@ -88,18 +64,8 @@
             submit(event) {
                 event.preventDefault()
 
-                // todo use vee validete
-
-                // if (!this.email || !this.password)
-                //     return
-                //
-                // if (this.password !== this.confirm)
-                //     return
-
-                if (this.form === 0) {
-                    this.form = 1
+                if (this.errors.any())
                     return
-                }
 
                 const data = {
                     email: this.email,
